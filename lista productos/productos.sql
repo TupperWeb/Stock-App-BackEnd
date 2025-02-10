1d58b2d6460dbeb5bc6ac585db6a49a48d569911
Paula General <generalpaulawrk@gmail.com>
2025-02-10 18:14:01 -0300

    Update tablas
    - Agregué la columna precio en la tabla Productos
    - Cambié algunos TEXT a VARCHAR, ya que no escribiremos descripciones ni textos muy largos.
    - Agregué la tabla Pedido que contiene su propio id, el código del producto, la cantidad, el precio total (cantidad * precio unitario) y la fecha en que se realizó el pedido.
    
    Mejorar:
    - En un mismo pedido debe entrar más de un producto.
    - Código del producto como UUID
    - Tabla de pedidos pendientes

--- lista productos/productos.sql
+++ lista productos/productos.sql
@@ -1,43 +1,53 @@
-CREATE TABLE categorias (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT UNIQUE NOT NULL
+CREATE TABLE Categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE productos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    codigo TEXT UNIQUE NOT NULL,
-    nombre TEXT NOT NULL,
-    stock INTEGER NOT NULL,
-    categoria_id INTEGER NOT NULL,
-    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+CREATE TABLE Productos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    codigo INT UNIQUE NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    stock INT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES Categorias(id) ON UPDATE CASCADE
 );
 
-INSERT INTO categorias (nombre) VALUES ('bebidas'), ('comida');
+CREATE TABLE Pedido (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  codigoProducto INT,
+  cantidad INT NOT NULL CHECK(cantidad > 0),
+  precioTotal DECIMAL(10,2) NOT NULL,
+  fecha DATE NOT NULL,
+  FOREIGN KEY (codigoProducto) REFERENCES Productos(codigo) ON UPDATE CASCADE
+);
 
-INSERT INTO productos (codigo, nombre, stock, categoria_id) VALUES
-('10001', 'Coca-Cola 500ml', 50, 1),
-('10002', 'Agua Mineral 1L', 30, 1),
-('10003', 'Coca-Cola Sin Azúcar 500ml', 40, 1),
-('10004', 'Fanta 500ml', 35, 1),
-('10005', 'Agua Mineral 500ml', 25, 1),
-('10006', 'Agua Saborizada 1L', 30, 1),
-('20001', 'Pan lactal', 20, 2),
-('20002', 'Galletas dulces', 40, 2),
-('20003', 'Queso', 15, 2),
-('20004', 'Fruta', 30, 2),
-('20005', 'Pollo', 25, 2),
-('20006', 'Carne', 20, 2),
-('20007', 'Condimentos de ensalada', 50, 2),
-('20008', 'Verdura', 35, 2),
-('20009', 'Especias', 40, 2),
-('20010', 'Sal', 100, 2),
-('20011', 'Aceite', 30, 2),
-('20012', 'Azúcar', 50, 2),
-('20013', 'Pescado', 15, 2),
-('20014', 'Huevo', 60, 2),
-('20015', 'Jamón', 20, 2),
-('20016', 'Mayonesa', 25, 2),
-('20017', 'Mostaza', 25, 2),
-('20018', 'Ketchup', 25, 2),
-('20019', 'Harina', 40, 2),
-('20020', 'Levadura', 50, 2); 
+INSERT INTO Categorias (nombre) VALUES ('bebidas'), ('comida');
+
+INSERT INTO Productos (codigo, nombre, stock, precio, categoria_id) VALUES
+('10001', 'Coca-Cola 500ml', 50, 1200, 1),
+('10002', 'Agua Mineral 1L', 30, 1500, 1),
+('10003', 'Coca-Cola Sin Azúcar 500ml', 40, 1200, 1),
+('10004', 'Fanta 500ml', 35, 1200, 1),
+('10005', 'Agua Mineral 500ml', 25, 850, 1),
+('10006', 'Agua Saborizada 1L', 30, 2500, 1),
+('20001', 'Pan lactal', 20, 3000, 2),
+('20002', 'Galletas dulces', 40, 1350, 2),
+('20003', 'Queso', 15, 4000, 2),
+('20004', 'Fruta', 30, 1160, 2),
+('20005', 'Pollo', 25, 4600, 2),
+('20006', 'Carne', 20, 5200, 2),
+('20007', 'Condimentos de ensalada', 50, 700, 2),
+('20008', 'Verdura', 35, 2000, 2),
+('20009', 'Especias', 40, 750, 2),
+('20010', 'Sal', 100, 1200, 2),
+('20011', 'Aceite', 30, 3000, 2),
+('20012', 'Azúcar', 50, 1600, 2),
+('20013', 'Pescado', 15, 5800, 2),
+('20014', 'Huevo', 60, 6000, 2),
+('20015', 'Jamón', 20, 2200, 2),
+('20016', 'Mayonesa', 25, 2700, 2),
+('20017', 'Mostaza', 25, 2500, 2),
+('20018', 'Ketchup', 25, 2700, 2),
+('20019', 'Harina', 40, 1700, 2),
+('20020', 'Levadura', 50, 1000, 2); 
