722f9f22c00a79f178148479195305cdf31eb1a4
Paula General <generalpaulawrk@gmail.com>
2025-02-14 20:40:24 -0300

    Tabla Reponer_Stock
    - Añadí la tabla Reponer_Stock que contiene su propio id, el id del producto a reponer y la cantidad. Se insertaran datos en ella cuando se desee realizar un pedido y no haya stock suficiente del producto.
    - Modifiqué el atributo codigo de la tabla Productos como UUID, el cual se genera automáticamente.
    - Agregué la función CURRENT_DATE que genera la fecha actual.

--- lista productos/productos.sql
+++ lista productos/productos.sql
@@ -1,3 +1,4 @@
+-- 1. CREACIÓN DE TABLAS
 CREATE TABLE Categorias (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50) UNIQUE NOT NULL
@@ -5,7 +6,7 @@ CREATE TABLE Categorias (
 
 CREATE TABLE Productos (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    codigo INT UNIQUE NOT NULL,
+    codigo BINARY(16) NOT NULL DEFAULT (UUID_TO_BIN(UUID())) UNIQUE, -- Genera un codigo para identificar cada producto
     nombre VARCHAR(50) NOT NULL,
     stock INT NOT NULL,
     precioUnitario DECIMAL(10,2) NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE Productos (
 CREATE TABLE Pedido ( -- Contiene información general de el pedido
   id INT PRIMARY KEY AUTO_INCREMENT,
   precioTotal DECIMAL(10,2) NOT NULL, -- Precio total de todo el pedido
-  fecha DATE NOT NULL  
+  fecha DATE NOT NULL DEFAULT (CURRENT_DATE) -- Inserta la fecha actual 
 );
 
 CREATE TABLE Pedido_Productos( -- Contiene informacion especifica de cada producto pedido
@@ -29,32 +30,46 @@ CREATE TABLE Pedido_Productos( -- Contiene informacion especifica de cada produc
     FOREIGN KEY (idPedido) REFERENCES Pedido(id) ON UPDATE CASCADE
 );
 
-INSERT INTO Categorias (nombre) VALUES ('bebidas'), ('comida');
+CREATE TABLE Reponer_Stock ( -- Cuando se quiere realizar un pedido y no hay stock suficiente se carga el producto y la cantidad a reponer. (tendris que tener el id dep pedido? o el pedido no se realiza)
+id INT PRIMARY KEY AUTO_INCREMENT,
+idProducto INT UNIQUE,  -- Se mantiene único por producto para evitar duplicados
+cantidad INT NOT NULL CHECK(cantidad > 0),
+FOREIGN KEY (idProducto) REFERENCES Productos(id) ON UPDATE CASCADE
+);
 
-INSERT INTO Productos (codigo, nombre, stock, precio, categoria_id) VALUES
-('10001', 'Coca-Cola 500ml', 50, 1200, 1),
-('10002', 'Agua Mineral 1L', 30, 1500, 1),
-('10003', 'Coca-Cola Sin Azúcar 500ml', 40, 1200, 1),
-('10004', 'Fanta 500ml', 35, 1200, 1),
-('10005', 'Agua Mineral 500ml', 25, 850, 1),
-('10006', 'Agua Saborizada 1L', 30, 2500, 1),
-('20001', 'Pan lactal', 20, 3000, 2),
-('20002', 'Galletas dulces', 40, 1350, 2),
-('20003', 'Queso', 15, 4000, 2),
-('20004', 'Fruta', 30, 1160, 2),
-('20005', 'Pollo', 25, 4600, 2),
-('20006', 'Carne', 20, 5200, 2),
-('20007', 'Condimentos de ensalada', 50, 700, 2),
-('20008', 'Verdura', 35, 2000, 2),
-('20009', 'Especias', 40, 750, 2),
-('20010', 'Sal', 100, 1200, 2),
-('20011', 'Aceite', 30, 3000, 2),
-('20012', 'Azúcar', 50, 1600, 2),
-('20013', 'Pescado', 15, 5800, 2),
-('20014', 'Huevo', 60, 6000, 2),
-('20015', 'Jamón', 20, 2200, 2),
-('20016', 'Mayonesa', 25, 2700, 2),
-('20017', 'Mostaza', 25, 2500, 2),
-('20018', 'Ketchup', 25, 2700, 2),
-('20019', 'Harina', 40, 1700, 2),
-('20020', 'Levadura', 50, 1000, 2); 
+-- 2. INSERTAR DATOS PRINCIPALES
+
+INSERT INTO Categorias (nombre) VALUES 
+('bebidas'), 
+('comida');
+
+INSERT INTO Productos (nombre, stock, precioUnitario, categoria_id) VALUES --No insertamos el codigo, ya que SQL lo genera automaticamente con (UUID_TO_BIN(UUID()))
+('Coca-Cola 500ml', 50, 1200, 1),
+('Agua Mineral 1L', 30, 1500, 1),
+('Coca-Cola Sin Azúcar 500ml', 40, 1200, 1),
+('Fanta 500ml', 35, 1200, 1),
+('Agua Mineral 500ml', 25, 850, 1),
+('Agua Saborizada 1L', 30, 2500, 1),
+('Pan lactal', 20, 3000, 2),
+('Galletas dulces', 40, 1350, 2),
+('Queso', 15, 4000, 2),
+('Fruta', 30, 1160, 2),
+('Pollo', 25, 4600, 2),
+('Carne', 20, 5200, 2),
+('Condimentos de ensalada', 50, 700, 2),
+('Verdura', 35, 2000, 2),
+('Especias', 40, 750, 2),
+('Sal', 100, 1200, 2),
+('Aceite', 30, 3000, 2),
+('Azúcar', 50, 1600, 2),
+('Pescado', 15, 5800, 2),
+('Huevo', 60, 6000, 2),
+('Jamón', 20, 2200, 2),
+('Mayonesa', 25, 2700, 2),
+('Mostaza', 25, 2500, 2),
+('Ketchup', 25, 2700, 2),
+('Harina', 40, 1700, 2),
+('Levadura', 50, 1000, 2);
+
+-- 3. PROCEDIMIENTO: Se encargará de verificar si hay stock del producto pedido, si no hay inserta los datos en la tabla Reponer_Stock
+
